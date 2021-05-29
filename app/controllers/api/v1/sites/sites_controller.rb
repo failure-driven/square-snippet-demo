c942766e0d035c68920781c54a85b1fbcd341f22
Luca Cave <lucacave2@gmail.com>
2021-05-29 17:55:57 +1000

    check if site not null before getting visit count

--- app/controllers/api/v1/sites/sites_controller.rb
+++ app/controllers/api/v1/sites/sites_controller.rb
@@ -4,13 +4,13 @@ module Api
       class SitesController < BaseApiController
         def visit_count
           site = Site.find_by(domain: params[:domain])
-          render json: { visit_count: site.visit_count }
+          render json: { visit_count: site.visit_count } if site
         end
 
         def update_visit_count
           site = Site.find_by(domain: params[:domain])
           site.update!(visit_count: site.visit_count + 1)
-          render json: { visit_count: site.visit_count }
+          render json: { visit_count: site.visit_count } if site
         end
       end
     end
