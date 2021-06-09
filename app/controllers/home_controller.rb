2a96d264be323d5080d8fe60086422f8358ab9c9
Anton Panteleev <panteleev23@gmail.com>
2021-06-09 15:51:28 +1000

    CLEANUP: Privacy page. common variables setter for T&C + Privacy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,30 +1,23 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: %i[terms privacy]
+  before_action :set_company_details
 
   def index
     @identity = current_user.identities.first
   end
 
-  def terms
-    company_details
-    @update_date_tnc = "June 08, 2021"
-  end
-
-  def privacy
-    company_details
-    @update_date_privacy = "June 07, 2021"
-  end
-
   private
 
-  def company_details
-    @website_address = "https://stg-swif.herokuapp.com/"
+  def set_company_details
+    @website_address = "https://swif.club"
     @company_name = "Swif"
     @country_of_hosting = "USA"
     @privacy_country = "USA"
     @contact_detail_first = "_________"
     @contact_detail_second = "_________"
-    @contact_detail_email = "email@address.com"
+    @contact_detail_email = "swif.snippet@gmail.com"
     @contact_detail_misc = "_________"
+    @update_date_tnc = "June 08, 2021"
+    @update_date_privacy = "June 07, 2021"
   end
 end
