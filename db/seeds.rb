69d9dfeebaa693ff8feb7b01a17f220979f2b4cf
Michael Milewski <saramic@gmail.com>
2021-06-16 22:58:25 +1000

    idempotent db:seeds to keep toggles upto date
    old toggles get deleted, new ones get added
    
    now just need to make rails db:seed part of build and deploy!
    
    Co-authored-by: Anton Panteleev <panteleev23@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TOGGLES = %i[
+  protal
+  user_nav
+].freeze
+
+TOGGLES.each do |toggle|
+  Flipper[toggle].add
+end
+
+(Flipper.features.map(&:key).map(&:to_sym) - TOGGLES).each do |old_toggle|
+  Flipper[old_toggle].remove
+end
