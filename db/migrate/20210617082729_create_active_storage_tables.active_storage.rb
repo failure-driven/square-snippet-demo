2704b9d79cff6108766bca17602f7abf7e8735b1
Selena Small <selenawiththetattoo@gmail.com>
2021-06-18 08:52:41 +1000

    cop

--- db/migrate/20210617082729_create_active_storage_tables.active_storage.rb
+++ db/migrate/20210617082729_create_active_storage_tables.active_storage.rb
@@ -26,6 +26,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
 
+    # rubocop:disable Rails/CreateTableWithTimestamps
     create_table :active_storage_variant_records, id: :uuid do |t|
       t.belongs_to :blob, null: false, index: false, type: :uuid
       t.string :variation_digest, null: false
@@ -33,5 +34,6 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.index %i[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
+    # rubocop:enable Rails/CreateTableWithTimestamps
   end
 end
