4507ad50e3b561a5c920441e852499f788a82659
Anton Panteleev <panteleev23@gmail.com>
2021-06-26 23:17:08 +1000

    FIX: rubocop + frontend

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -2,16 +2,24 @@ module AvatarHelper
   def avatar_path(object, options = {})
     size = options[:size] || 180
     default_image = options[:default] || "mp"
-    base_url =  "https://secure.gravatar.com/avatar"
+    base_url = "https://secure.gravatar.com/avatar"
     base_url_params = "?s=#{size}&d=#{default_image}"
 
-    if object.respond_to?(:avatar) && object.avatar.attached? && object.avatar.variable?
-      object.avatar.variant(resize_to_fill: [size, size, { gravity: 'Center' }])
-    elsif object.respond_to?(:email) && object.email
-      gravatar_id = Digest::MD5::hexdigest(object.email.downcase)
+    if respond_to_avatar?(object)
+      object.avatar.variant(resize_to_fill: [size, size, { gravity: "Center" }])
+    elsif respond_to_email?(object)
+      gravatar_id = Digest::MD5.hexdigest(object.email.downcase)
       "#{base_url}/#{gravatar_id}#{base_url_params}"
     else
       "#{base_url}/00000000000000000000000000000000#{base_url_params}"
     end
+  end
+
+  def respond_to_avatar?(object)
+    object.respond_to?(:avatar) && object.avatar.attached? && object.avatar.variable?
+  end
+
+  def respond_to_email?(object)
+    object.respond_to?(:email) && object.email
   end
 end
