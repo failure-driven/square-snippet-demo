e61d1a4b90f515e6e873d1d0c23394e61d1c74d8
Anton Panteleev <panteleev23@gmail.com>
2021-06-26 19:58:46 +1000

    BACK+FRONT: user can upload avatar
    user has one avatar. implemented via devise controller / custom devise view.

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem "jbuilder", "~> 2.7"
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+# Simple form - a better way of doing form_for
+gem 'simple_form'
 
 # Use Active Storage variant
 gem "image_processing", "~> 1.2"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,4 +46,12 @@ class ApplicationController < ActionController::Base
 
     jwt_payload["id"]
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+  end
 end
