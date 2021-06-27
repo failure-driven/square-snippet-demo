4464c372e83e091eb86f50d81c08f6e6be39b54b
Michael Milewski <saramic@gmail.com>
2021-06-27 13:48:08 +1000

    a way to re-inject app on any site in dev tools
    by providing an accessable varialble swifApp.app()

--- app/javascript/packs/widget_demo_svelte.js
+++ app/javascript/packs/widget_demo_svelte.js
@@ -1,19 +1,27 @@
 import App from '../src/svelte/app.svelte'
 
 let element = document.querySelector('[data-widget=demo-widget]');
-if(!element) {
+if (!element) {
   element = document.createElement("div");
   element.setAttribute("data-widget", "demo-widget");
   document.body.appendChild(element);
 }
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new App({
-    target: element,
-    props: {
-      name: 'Svelte'
-    }
-  });
+var swifApp = (function (my) {
+  my.app = () => {
+    var app = new App({
+      target: element,
+      props: {
+        name: 'Svelte'
+      }
+    });
 
-  window.app = app;
+    window.app = app;
+    return app;
+  };
+  return (my)
+})(swifApp || {})
+window.swifApp = swifApp;
+document.addEventListener('DOMContentLoaded', () => {
+  window.swifApp.app()
 })
 
